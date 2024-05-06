c003312416db3a96c6a22017e74f41926be964ec
Nycolas <nybertocco@gmail.com>
2024-05-06 20:00:23 -0300

    projeto finalizado

--- API/routes/index.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express")
-
-const usersRoutes = require("./users.routes")
-
-const routes = Router()
-routes.use("/users", usersRouter)
-
-module.exports = routes
---
+++ Agenda/script.js
@@ -0,0 +1,7 @@
+const Btn = document.getElementById('Mode')
+
+Btn.addEventListener('click', function() {
+    document.documentElement.classList.toggle('light')
+}) 
+    
+
